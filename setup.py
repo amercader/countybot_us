93c9f79f87ce4b9cc1eb9972c3179b3ca57bc140
amercader <amercadero@gmail.com>
2021-12-05 21:43:10 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="countybot_us",
-    version="0.0.2",
+    version="0.0.3",
     author="Adrià Mercader",
     author_email="amercadero@gmail.com",
     description="A Twitter bot built with munibot that tweets imagery of US Counties and equivalent units",
